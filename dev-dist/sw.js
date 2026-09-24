30d8d297edcd37c4fe4e70fe14a9c438ec800593
Anvexaa AI <subhashbadgoti784e@gmail.com>
2026-09-24 17:43:04 +0000

    Update GitHub modal with subhashbadgoti784e-cloud repository

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kt0sfh2u7d8"
+    "revision": "0.llbf49d8q0o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
